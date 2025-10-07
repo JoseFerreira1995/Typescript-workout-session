7040675dccb8401486b19fb2d6f68e19f5081cc4
Jose Ferreira <jpedroferreira95@gmail.com>
2025-10-07 10:39:20 +0100

    feat:add template

---
+++ basic-exercises/exercices.ts
@@ -0,0 +1,31 @@
+// === Basic JavaScript & Algorithm Scripting ===
+
+// 1. Convert Celsius to Fahrenheit
+export function convertToF(celsius: number): number {
+   
+    throw new Error("Not implemented");
+  }
+  
+  // 2. Reverse a String
+  export function reverseString(str: string): string {
+   
+    throw new Error("Not implemented");
+  }
+  
+  // 3. Factorialize a Number
+  export function factorialize(num: number): number {
+  
+    throw new Error("Not implemented");
+  }
+  
+  // 4. Find the Longest Word in a String
+  export function findLongestWordLength(str: string): number {
+
+    throw new Error("Not implemented");
+  }
+  
+  // 5. Return Largest Numbers in Arrays
+  export function largestOfFour(arrays: number[][]): number[] {
+
+    throw new Error("Not implemented");
+  }
